2062d8f8228356ed553d13eb4fe660a65b7620d7
shelgeatharva27 <shelgeatharva27@gmail.com>
2024-11-07 00:26:54 +0530

    nain

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,152 +1,156 @@
-import { PROJECTS } from "../constants"
 const Projects = () => {
   return (
     <div className="border-b border-neutral-800 pb-4">
       <h2 className="my-20 text-center text-3xl md:text-4xl">Activities</h2>
 
-
+      {/* Tiger Spotting */}
       <div className="mb-8 flex flex-wrap items-center justify-center text-center">
-          <div className="w-full sm:w-3/4 md:w-1/3 lg:w-1/4 mb-4 mx-auto">
-            <a href="src/assets/projects/photo_6111484930744761707_y.jpg" target="_blank" rel="noopener noreferrer">
-              <img
-                src="src/assets/projects/photo_6111484930744761707_y.jpg"
-                alt="Project 4 Title"
-                className="w-full h-auto max-w-xs mb-6 rounded mx-auto"
-              />
-            </a>
-          </div>
-          <div className="w-full sm:w-3/4 md:w-2/3 lg:w-3/4 mx-auto text-center">
-            <h6 className="mb-2 font-semibold text-lg md:text-xl lg:text-3xl bg-gradient-to-r from-pink-300 to-purple-500 bg-clip-text text-3xl tracking-tight text-transparent lg:text-4xl">Tiger Spotting</h6>
-            <p className="mb-4 text-neutral-400 text-sm md:text-base lg:text-2xl">Join us for a tiger jungle safari—where the only thing roaring louder than the tigers is your excitement to see them! 🐅🌳.</p>
-
-          </div>
+        <div className="w-full sm:w-3/4 md:w-1/3 lg:w-1/4 mb-4 mx-auto">
+          <a href="/assets/projects/photo_6111484930744761707_y.jpg" target="_blank" rel="noopener noreferrer">
+            <img
+              src="/assets/projects/photo_6111484930744761707_y.jpg"
+              alt="Tiger Spotting"
+              className="w-full h-auto max-w-xs mb-6 rounded mx-auto"
+            />
+          </a>
         </div>
-
-        <div className="mb-8 flex flex-wrap items-center justify-center text-center">
-          <div className="w-full sm:w-3/4 md:w-1/3 lg:w-1/4 mb-4 mx-auto">
-            <a href="src/assets/projects/photo_6111484930744761649_y.jpg" target="_blank" rel="noopener noreferrer">
-              <img
-                src="src/assets/projects/photo_6111484930744761649_y.jpg"
-                alt="Project 3 Title"
-                className="w-full h-auto max-w-xs mb-6 rounded mx-auto"
-              />
-            </a>
-          </div>
-          <div className="w-full sm:w-3/4 md:w-2/3 lg:w-3/4 mx-auto text-center">
-            <h6 className="mb-2 font-semibold text-lg md:text-xl lg:text-3xl bg-gradient-to-r from-pink-300 to-purple-500 bg-clip-text text-3xl tracking-tight text-transparent lg:text-4xl">Vegitable Picking</h6>
-            <p className="mb-4 text-neutral-400 text-sm md:text-base lg:text-2xl">it's like a workout, but with way more snacks involved! 🍅💪</p>
-
-          </div>
+        <div className="w-full sm:w-3/4 md:w-2/3 lg:w-3/4 mx-auto text-center">
+          <h6 className="mb-2 font-semibold text-lg md:text-xl lg:text-3xl bg-gradient-to-r from-pink-300 to-purple-500 bg-clip-text text-transparent tracking-tight lg:text-4xl">
+            Tiger Spotting
+          </h6>
+          <p className="mb-4 text-neutral-400 text-sm md:text-base lg:text-2xl">
+            Join us for a tiger jungle safari—where the only thing roaring louder than the tigers is your excitement to see them! 🐅🌳.
+          </p>
         </div>
+      </div>
 
-        <div className="mb-8 flex flex-wrap items-center justify-center text-center">
-          <div className="w-full sm:w-3/4 md:w-1/3 lg:w-1/4 mb-4 mx-auto">
-            <a href="src/assets/projects/photo_6111484930744761662_y.jpg" target="_blank" rel="noopener noreferrer">
-              <img
-                src="src/assets/projects/photo_6111484930744761662_y.jpg"
-                alt="Project 1 Title"
-                className="w-full h-auto max-w-xs mb-6 rounded mx-auto"
-              />
-            </a>
-          </div>
-          <div className="w-full sm:w-3/4 md:w-2/3 lg:w-3/4 mx-auto text-center">
-            <h6 className="mb-2 font-semibold text-lg md:text-xl lg:text-3xl bg-gradient-to-r from-pink-300 to-purple-500 bg-clip-text text-3xl tracking-tight text-transparent lg:text-4xl">Cycling</h6>
-            <p className="mb-4 text-neutral-400 text-sm md:text-base lg:text-2xl">Hop on a bike and cruise the farm—where your biggest roadblock might be a curious cow! 🚴🐄.</p>
-
-          </div>
+      {/* Vegetable Picking */}
+      <div className="mb-8 flex flex-wrap items-center justify-center text-center">
+        <div className="w-full sm:w-3/4 md:w-1/3 lg:w-1/4 mb-4 mx-auto">
+          <a href="/assets/projects/photo_6111484930744761649_y.jpg" target="_blank" rel="noopener noreferrer">
+            <img
+              src="/assets/projects/photo_6111484930744761649_y.jpg"
+              alt="Vegetable Picking"
+              className="w-full h-auto max-w-xs mb-6 rounded mx-auto"
+            />
+          </a>
         </div>
-
-
-
-
-
-
-
-
-      <div>
-        {/* Project 1 */}
-
-
-        <div className="mb-8 flex flex-wrap items-center justify-center text-center">
-          <div className="w-full sm:w-3/4 md:w-1/3 lg:w-1/4 mb-4 mx-auto">
-            <a href="src/assets/projects/photo_6111484930744761659_y.jpg" target="_blank" rel="noopener noreferrer">
-              <img
-                src="src/assets/projects/photo_6111484930744761659_y.jpg"
-                alt="Project 1 Title"
-                className="w-full h-auto max-w-xs mb-6 rounded mx-auto"
-              />
-            </a>
-          </div>
-          <div className="w-full sm:w-3/4 md:w-2/3 lg:w-3/4 mx-auto text-center">
-            <h6 className="mb-2 font-semibold text-lg md:text-xl lg:text-3xl bg-gradient-to-r from-pink-300 to-purple-500 bg-clip-text text-3xl tracking-tight text-transparent lg:text-4xl">Volleyball</h6>
-            <p className="mb-4 text-neutral-400 text-sm md:text-base lg:text-2xl">Join our volleyball game—where we aim for the net, but sometimes end up hitting the nearest tree instead! 🌳🏐</p>
-
-          </div>
+        <div className="w-full sm:w-3/4 md:w-2/3 lg:w-3/4 mx-auto text-center">
+          <h6 className="mb-2 font-semibold text-lg md:text-xl lg:text-3xl bg-gradient-to-r from-pink-300 to-purple-500 bg-clip-text text-transparent tracking-tight lg:text-4xl">
+            Vegetable Picking
+          </h6>
+          <p className="mb-4 text-neutral-400 text-sm md:text-base lg:text-2xl">
+            It's like a workout, but with way more snacks involved! 🍅💪
+          </p>
         </div>
+      </div>
 
-       
-        {/* Project 3 */}
-
-        <div className="mb-8 flex flex-wrap items-center justify-center text-center">
-          <div className="w-full sm:w-3/4 md:w-1/3 lg:w-1/4 mb-4 mx-auto">
-            <a href="src/assets/projects/photo_6149698070821060736_y.jpg" target="_blank" rel="noopener noreferrer">
-              <img
-                src="src/assets/projects/photo_6149698070821060736_y.jpg"
-                alt="Project 3 Title"
-                className="w-full h-auto max-w-xs mb-6 rounded mx-auto"
-              />
-            </a>
-          </div>
-          <div className="w-full sm:w-3/4 md:w-2/3 lg:w-3/4 mx-auto text-center">
-            <h6 className="mb-2 font-semibold text-lg md:text-xl lg:text-3xl bg-gradient-to-r from-pink-300 to-purple-500 bg-clip-text text-3xl tracking-tight text-transparent lg:text-4xl">Bonfire</h6>
-            <p className="mb-4 text-neutral-400 text-sm md:text-base lg:text-2xl">Join us for a bonfire—where the only thing hotter than the flames is the gossip about the annoying boss at work or at home🔥😆</p>
-
-          </div>
+      {/* Cycling */}
+      <div className="mb-8 flex flex-wrap items-center justify-center text-center">
+        <div className="w-full sm:w-3/4 md:w-1/3 lg:w-1/4 mb-4 mx-auto">
+          <a href="/assets/projects/photo_6111484930744761662_y.jpg" target="_blank" rel="noopener noreferrer">
+            <img
+              src="/assets/projects/photo_6111484930744761662_y.jpg"
+              alt="Cycling"
+              className="w-full h-auto max-w-xs mb-6 rounded mx-auto"
+            />
+          </a>
         </div>
-
-
-
-        {/* Project 5 */}
-        <div className="mb-8 flex flex-wrap items-center justify-center text-center">
-          <div className="w-full sm:w-3/4 md:w-1/3 lg:w-1/4 mb-4 mx-auto">
-            <a href="src/assets/projects/photo_6111484930744761677_y.jpg" target="_blank" rel="noopener noreferrer">
-              <img
-                src="src/assets/projects/photo_6111484930744761677_y.jpg"
-                alt="Project 5 Title"
-                className="w-full h-auto max-w-xs mb-6 rounded mx-auto"
-              />
-            </a>
-          </div>
-          <div className="w-full sm:w-3/4 md:w-2/3 lg:w-3/4 mx-auto text-center">
-            <h6 className="mb-2 font-semibold text-lg md:text-xl lg:text-3xl bg-gradient-to-r from-pink-300 to-purple-500 bg-clip-text text-3xl tracking-tight text-transparent lg:text-4xl">Star Gazing</h6>
-            <p className="mb-4 text-neutral-400 text-sm md:text-base lg:text-2xl">Come stargaze with us! It’s like Netflix for your eyes, but with way less drama and way more twinkling! 🌟.</p>
-
-          </div>
-          <div className="mb-8 flex flex-wrap items-center justify-center text-center">
-          <div className="w-full sm:w-3/4 md:w-1/3 lg:w-1/4 mb-4 mx-auto">
-            <a href="src/assets/projects/photo_6149698070821060735_y.jpg" target="_blank" rel="noopener noreferrer">
-              <img
-                src="src/assets/projects/photo_6149698070821060735_y.jpg"
-                alt="Project 4 Title"
-                className="w-full h-auto max-w-xs mb-6 rounded mx-auto"
-              />
-            </a>
-          </div>
-          <div className="w-full sm:w-3/4 md:w-2/3 lg:w-3/4 mx-auto text-center">
-            <h6 className="mb-2 font-semibold text-lg md:text-xl lg:text-3xl bg-gradient-to-r from-pink-300 to-purple-500 bg-clip-text text-3xl tracking-tight text-transparent lg:text-4xl">Bullock cart </h6>
-            <p className="mb-4 text-neutral-400 text-sm md:text-base lg:text-2xl">Saddle up for a bullock cart adventure, where you’ll feel like a cowboy, minus the hat and the actual riding skills! 🤠🐄</p>
-
-          </div>
+        <div className="w-full sm:w-3/4 md:w-2/3 lg:w-3/4 mx-auto text-center">
+          <h6 className="mb-2 font-semibold text-lg md:text-xl lg:text-3xl bg-gradient-to-r from-pink-300 to-purple-500 bg-clip-text text-transparent tracking-tight lg:text-4xl">
+            Cycling
+          </h6>
+          <p className="mb-4 text-neutral-400 text-sm md:text-base lg:text-2xl">
+            Hop on a bike and cruise the farm—where your biggest roadblock might be a curious cow! 🚴🐄.
+          </p>
         </div>
+      </div>
+
+      {/* Volleyball */}
+      <div className="mb-8 flex flex-wrap items-center justify-center text-center">
+        <div className="w-full sm:w-3/4 md:w-1/3 lg:w-1/4 mb-4 mx-auto">
+          <a href="/assets/projects/photo_6111484930744761659_y.jpg" target="_blank" rel="noopener noreferrer">
+            <img
+              src="/assets/projects/photo_6111484930744761659_y.jpg"
+              alt="Volleyball"
+              className="w-full h-auto max-w-xs mb-6 rounded mx-auto"
+            />
+          </a>
+        </div>
+        <div className="w-full sm:w-3/4 md:w-2/3 lg:w-3/4 mx-auto text-center">
+          <h6 className="mb-2 font-semibold text-lg md:text-xl lg:text-3xl bg-gradient-to-r from-pink-300 to-purple-500 bg-clip-text text-transparent tracking-tight lg:text-4xl">
+            Volleyball
+          </h6>
+          <p className="mb-4 text-neutral-400 text-sm md:text-base lg:text-2xl">
+            Join our volleyball game—where we aim for the net, but sometimes end up hitting the nearest tree instead! 🌳🏐
+          </p>
+        </div>
+      </div>
+
+      {/* Bonfire */}
+      <div className="mb-8 flex flex-wrap items-center justify-center text-center">
+        <div className="w-full sm:w-3/4 md:w-1/3 lg:w-1/4 mb-4 mx-auto">
+          <a href="/assets/projects/photo_6149698070821060736_y.jpg" target="_blank" rel="noopener noreferrer">
+            <img
+              src="/assets/projects/photo_6149698070821060736_y.jpg"
+              alt="Bonfire"
+              className="w-full h-auto max-w-xs mb-6 rounded mx-auto"
+            />
+          </a>
+        </div>
+        <div className="w-full sm:w-3/4 md:w-2/3 lg:w-3/4 mx-auto text-center">
+          <h6 className="mb-2 font-semibold text-lg md:text-xl lg:text-3xl bg-gradient-to-r from-pink-300 to-purple-500 bg-clip-text text-transparent tracking-tight lg:text-4xl">
+            Bonfire
+          </h6>
+          <p className="mb-4 text-neutral-400 text-sm md:text-base lg:text-2xl">
+            Join us for a bonfire—where the only thing hotter than the flames is the gossip about the annoying boss at work or at home🔥😆
+          </p>
+        </div>
+      </div>
+
+      {/* Star Gazing */}
+      <div className="mb-8 flex flex-wrap items-center justify-center text-center">
+        <div className="w-full sm:w-3/4 md:w-1/3 lg:w-1/4 mb-4 mx-auto">
+          <a href="/assets/projects/photo_6111484930744761677_y.jpg" target="_blank" rel="noopener noreferrer">
+            <img
+              src="/assets/projects/photo_6111484930744761677_y.jpg"
+              alt="Star Gazing"
+              className="w-full h-auto max-w-xs mb-6 rounded mx-auto"
+            />
+          </a>
+        </div>
+        <div className="w-full sm:w-3/4 md:w-2/3 lg:w-3/4 mx-auto text-center">
+          <h6 className="mb-2 font-semibold text-lg md:text-xl lg:text-3xl bg-gradient-to-r from-pink-300 to-purple-500 bg-clip-text text-transparent tracking-tight lg:text-4xl">
+            Star Gazing
+          </h6>
+          <p className="mb-4 text-neutral-400 text-sm md:text-base lg:text-2xl">
+            Come stargaze with us! It’s like Netflix for your eyes, but with way less drama and way more twinkling! 🌟.
+          </p>
+        </div>
+      </div>
+
+      {/* Bullock Cart */}
+      <div className="mb-8 flex flex-wrap items-center justify-center text-center">
+        <div className="w-full sm:w-3/4 md:w-1/3 lg:w-1/4 mb-4 mx-auto">
+          <a href="/assets/projects/photo_6149698070821060735_y.jpg" target="_blank" rel="noopener noreferrer">
+            <img
+              src="/assets/projects/photo_6149698070821060735_y.jpg"
+              alt="Bullock Cart"
+              className="w-full h-auto max-w-xs mb-6 rounded mx-auto"
+            />
+          </a>
+        </div>
+        <div className="w-full sm:w-3/4 md:w-2/3 lg:w-3/4 mx-auto text-center">
+          <h6 className="mb-2 font-semibold text-lg md:text-xl lg:text-3xl bg-gradient-to-r from-pink-300 to-purple-500 bg-clip-text text-transparent tracking-tight lg:text-4xl">
+            Bullock Cart
+          </h6>
+          <p className="mb-4 text-neutral-400 text-sm md:text-base lg:text-2xl">
+            Take a ride on a bullock cart! It's Uber, but way slower and with way more mooing. 🐂🚜.
+          </p>
         </div>
       </div>
     </div>
-
-
-
   );
 };
-
-
 
 export default Projects;
